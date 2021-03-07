592cafa0421209d897beac789f254da68fa254d1
GolovinaElia <eliagolovina@ukr.net>
2021-03-08 00:03:42 +0200

    Добавляет файлы

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,3 @@
-// Перепиши функцию toggleUserState() так,
-// чтобы она не использовала callback - функцию callback,
-// а принимала всего два параметра allUsers и userName и возвращала промис.
-
 const users = [
   { name: 'Mango', active: true },
   { name: 'Poly', active: false },
@@ -13,7 +9,7 @@ const toggleUserState = (allUsers, userName) => {
 const updatedUsers = allUsers.map(user =>
     user.name === userName ? { ...user, active: !user.active } : user,
 );
-  return Promise.resolve({ allUsers, userName });
+  return Promise.resolve(updatedUsers);
   };
   
 const logger = updatedUsers => console.table(updatedUsers);
--- js/task-3.js
+++ js/task-3.js
@@ -1,40 +1,35 @@
-// Перепиши функцию makeTransaction() так, чтобы она не использовала
-// callback - функции onSuccess и onError, а принимала всего один параметр
-// transaction и возвращала промис.
-
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-const makeTransaction = (transaction, onSuccess, onError) => {
+const makeTransaction = (transaction) => {
   const delay = randomIntegerFromInterval(200, 500);
-
-  setTimeout(() => {
-    const canProcess = Math.random() > 0.3;
-
-    if (canProcess) {
-      onSuccess(transaction.id, delay);
-    } else {
-      onError(transaction.id);
-    }
-  }, delay);
+  return new Promise((resolve, reject) => {
+    let id = transaction.id;
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+      if (canProcess) {
+        resolve({ id, delay });
+      } else {
+        reject(transaction.id);
+      }
+    }, delay);
+  });
 };
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = (amount) => {
+  console.log(`Transaction ${amount.id} processed in ${amount.delay}ms`);
 };
 
 const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
-
 /*
  * Работает так
  */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
 /*
  * Должно работать так
  */
