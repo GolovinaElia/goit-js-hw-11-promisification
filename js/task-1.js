4a58d6d6850a7c95a6c82bbeb5fbf44d8504237e
GolovinaElia <eliagolovina@ukr.net>
2021-03-11 22:24:24 +0200

    Добавляет файлы

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,11 @@
 const delay = ms => {
-  return Promise.resolve(ms);
-};
+  return new Promise(resolve => {
+ setTimeout(() => {
+    resolve(ms);
+  }, delay);
+  })    
+ }
+ 
 const logger = time => console.log(`Resolved after ${time}ms`);
 
 delay(2000).then(logger); 
